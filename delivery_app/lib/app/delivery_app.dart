cc11119f321606290c87fe9aa374da81708a511a
Dos Reis, Vagner <vagnereis10@gmail.com>
2023-06-25 18:17:26 -0300

    Third class incompleted

--- delivery_app/lib/app/delivery_app.dart
+++ delivery_app/lib/app/delivery_app.dart
@@ -1,6 +1,7 @@
 import 'package:delivery_app/app/core/provider/application_binding.dart';
 import 'package:delivery_app/app/core/ui/theme/theme_config.dart';
 import 'package:delivery_app/app/pages/home/home_router.dart';
+import 'package:delivery_app/app/pages/product_detail/product_detail_router.dart';
 import 'package:delivery_app/app/pages/splash/splash_page.dart';
 import 'package:flutter/material.dart';
 
@@ -11,11 +12,13 @@ class DeliveryApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ApplicationBinding(
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Delivery App',
         theme: ThemeConfig.theme,
         routes: {
           '/': (context) => const SplashPage(),
           '/home': (context) => HomeRouter.page,
+          '/productDetail': (context) => ProductDetailRouter.page,
         },
       ),
     );
--- delivery_app/lib/app/pages/home/home_state.dart
+++ delivery_app/lib/app/pages/home/home_state.dart
@@ -1,4 +1,5 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:delivery_app/app/dto/order_product_dto.dart';
 import 'package:delivery_app/app/models/product_model.dart';
 import 'package:equatable/equatable.dart';
 import 'package:match/match.dart';
@@ -17,30 +18,35 @@ class HomeState extends Equatable {
   final String? errorMessage;
   final HomeStateStatus status;
   final List<ProductModel> products;
+  final List<OrderProductDto> shoppingBag;
 
   const HomeState({
     this.errorMessage,
     required this.status,
     required this.products,
+    required this.shoppingBag,
   });
 
   const HomeState.initial()
       : status = HomeStateStatus.initial,
         products = const [],
+        shoppingBag = const [],
         errorMessage = null;
 
   @override
-  List<Object?> get props => [status, products, errorMessage];
+  List<Object?> get props => [status, products, errorMessage, shoppingBag];
 
   HomeState copyWith({
     String? errorMessage,
     HomeStateStatus? status,
     List<ProductModel>? products,
+    List<OrderProductDto>? shoppingBag,
   }) {
     return HomeState(
       errorMessage: errorMessage ?? this.errorMessage,
       status: status ?? this.status,
       products: products ?? this.products,
+      shoppingBag: shoppingBag ?? this.shoppingBag,
     );
   }
 }
